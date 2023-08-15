1dd27578998fafae76295fbb1d16147ef29cdfb9
MOOW <christianfeliks1@gmail.com>
2023-08-16 00:56:56 +0200

    start implementing API get request

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL_TOP = "https://api.jikan.moe/v4/top/anime?type=tv&filter=airing";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    /* res.render("index.ejs"); */
+    try{
+        const result = await axios.get(API_URL_TOP);
+        console.log(result.data.data[0]); //! WTF IS THIS
+        //console.log(result.pegination.last_visible_page);
+        res.render("index.ejs");
+    } catch (error){
+        res.render("index.ejs");/*  */
+    }
+
 });
 
 app.listen(port, () => {
